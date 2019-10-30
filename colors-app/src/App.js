49710b9cdf477347f84d65d6ef284269bc7173cc
Nikolay <coyg1414@gmail.com>
2019-10-30 21:02:02 +0200

    Add Link to Palette

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -12,13 +12,13 @@ class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <PaletteList palettes={seedColors} />}/>
+        <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps}/>}/>
         <Route 
           exact 
           path="/palette/:id" 
-          render={(routeParams) => (
+          render={(routeProps) => (
             <Palette 
-              palette={generatePalette(this.findPalette(routeParams.match.params.id))} 
+              palette={generatePalette(this.findPalette(routeProps.match.params.id))} 
             />
           ) 
         }/>
