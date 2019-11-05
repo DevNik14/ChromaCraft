7b616b16b5a68a0679922eada33d3a520465eaab
DevNik14 <coyg1414@gmail.com>
2019-11-05 20:46:26 +0200

    Add SavePalette

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -8,14 +8,28 @@ import SingleColorPalette from './SingleColorPalette';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {palettes: seedColors};
+        this.savePalette = this.savePalette.bind(this);
+        this.findPalette = this.findPalette.bind(this);
+    }
   findPalette(id) {
-    return seedColors.find(palette => palette.id === id);
+    return this.state.palettes.find(palette => palette.id === id);
+  }
+  savePalette(newPalette) {
+      this.setState({palettes: [...this.state.palettes, newPalette]});
   }
   render() {
     return (
       <Switch>
-        <Route exact path="/palette/new" render={() => <NewPaletteForm />}/>
-        <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps}/>}/>
+        <Route
+            exact
+            path="/palette/new"
+            render={(routeProps) => <NewPaletteForm {...routeProps} savePalette={this.savePalette}/>}
+        />
+        <Route exact path="/" render={(routeProps) => <PaletteList palettes={this.state.palettes} {...routeProps}/>}/>
         <Route 
           exact 
           path="/palette/:id" 
